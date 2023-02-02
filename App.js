a0b033282815ab1cbb8b1559a42276dd6f278b13
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-02-02 12:55:07 -0600

    Initial Init; Installed React Native Navigation Packages; Drawer Package

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gatheroo, by Enterface</Text>
       <StatusBar style="auto" />
     </View>
   );
